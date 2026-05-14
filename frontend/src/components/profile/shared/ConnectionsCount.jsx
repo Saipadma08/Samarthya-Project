1a7db6aa0bbca89a566a93dffac5839326e2ecff
Lopa <lopamudra.mahapatra.lm@gmail.com>
2026-05-15 00:00:07 +0530

    Solved connections count problem

--- frontend/src/components/profile/shared/ConnectionsCount.jsx
+++ frontend/src/components/profile/shared/ConnectionsCount.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ConnectionsCount = () => {
+const ConnectionsCount = ({ userId }) => {
 
     const [connectionsCount, setConnectionsCount] = useState(0);
 
-    const loggedInUser = JSON.parse(localStorage.getItem("user"));
-
-
     useEffect(() => {
 
-        fetchConnectionsCount();
+        if(userId){
 
-    }, [loggedInUser]);
+            fetchConnectionsCount();
+
+        }
+
+    }, [userId]);
+
 
     async function fetchConnectionsCount() {
 
@@ -22,7 +23,7 @@ const ConnectionsCount = () => {
             const res =
                 await axios.get(
 
-                    `http://localhost:3000/api/connections/count/${loggedInUser.id}`,
+                    `http://localhost:3000/api/connections/count/${userId}`,
 
                     {
                         withCredentials: true
@@ -45,27 +46,35 @@ const ConnectionsCount = () => {
     }
 
     return (
+
         <div className="flex gap-6">
 
             <div>
 
                 <p className="font-bold text-lg">
+
                     {connectionsCount}
+
                 </p>
 
-                { connectionsCount === 1 ? 
-                    <p className="text-sm text-gray-500">
-                        Connection
-                    </p> : 
-                    <p className="text-sm text-gray-500">
-                        Connections
-                    </p>
-                }
+                <p className="text-sm text-gray-500">
+
+                    {
+                    connectionsCount === 1
+                    ?
+                    "Connection"
+                    :
+                    "Connections"
+                    }
+
+                </p>
 
             </div>
 
         </div>
-    )
+
+    );
+
 }
 
-export default ConnectionsCount
+export default ConnectionsCount;
